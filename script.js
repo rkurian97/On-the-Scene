35005a12bf722c142bd4e21ef08ca33e9edb495c
dakota <kotalilyy@gmail.com>
2021-03-26 11:55:01 -0500

    fixed js link

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-$.ajax({
-    url: "https://api.themoviedb.org/3/movie/550?api_key=4a89b391bd7e8061087938f4b3404220",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+alert("Hello");
