affcaa9e59cb471a27fa838554cb11b5fb9a81c0
rkurian <kurian.rohit1229@gmail.com>
2021-04-01 17:26:07 -0500

    comments, clean code, bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
+// urls
 const baseURL= 'https://api.themoviedb.org/3/';
 const baseImgUrl= "https://image.tmdb.org/t/p/";
 const smallPosterURL= 'w185';
 const bigPosterURL= 'w342';
-let key=0;
-if(localStorage){
-    key=localStorage.length;
-}                                                  
 
 // w45, w92, w152, w185, w342, w500, w780  poster sizes
 let query="jaws";
@@ -37,21 +34,26 @@ const none= document.getElementById("none");
 searchButton.addEventListener("click", function(){
     query= searchText.value; 
     queryContainer.innerHTML= "" 
+    // find movies is the function that does the api call
     findMovies();
 });
 
-//onclick function for each poster
+//onclick function for each poster. this function populates the modal with the information based on the movie that was clicked. 
 let activateModal= function(title, overview, rating, bigPosterPath, releaseDate, smallPosterPath, id){
+    //setting inner html
     modalTitle.innerHTML= title;
     modalOverview.innerHTML= overview;
     modalRating.innerHTML= modalRating.innerHTML+ rating;
     modalReleaseDate.innerHTML=modalReleaseDate.innerHTML+releaseDate;
     modalPoster.setAttribute("src", bigPosterPath);
     movieModal.setAttribute("style", "display:block");
+
+    //passing in the poster path as an attribute on the favorite button, so the recordFavroite function can record it into local storage if it was clicked. 
     smallPosterPath=JSON.stringify(smallPosterPath);
     favoriteButton.setAttribute("data-id", JSON.stringify(id));
     favoriteButton.setAttribute("onclick","recordFavorite("+smallPosterPath+")")
 
+    //every time a new poster is clicked initializing the display of the streaming logos to none. When the availability button is clicked it will repopulate based on the new movie clicked
     hbo.setAttribute("style", "display: none");
     netflix.setAttribute("style", "display: none");
     prime.setAttribute("style", "display: none");
@@ -62,11 +64,12 @@ let activateModal= function(title, overview, rating, bigPosterPath, releaseDate,
 
 //search query function
 let findMovies= function (){
-    fetch(baseURL+'search/movie'+apiKey+'&query='+query)
+    // api call
+    fetch(`${baseURL}search/movie${apiKey}&query=${query}`)
     .then(response => response.json())
     .then( function(data){
-        //loops through each object in the results response to show all posters
         console.log(data);
+        //loops through each object in the results response to show all posters
         for (i=0; i<data.results.length; i++){
             // if the movie does not have a poster we do not add to the results
             if(data.results[i].poster_path){
@@ -80,12 +83,13 @@ let findMovies= function (){
                 gradientDiv.setAttribute("class", "gradientDiv");
                 imgDiv.append(gradientDiv);
                 
-                //making poster and adding its data into the activateModal function
+                //making poster
                 const poster=document.createElement("img");
                 let smallPosterPath=data.results[i].poster_path;
                 poster.setAttribute("src", baseImgUrl+smallPosterURL+smallPosterPath);
                 poster.setAttribute("class", "smallPoster");
 
+                //adding all the necessary data into the activateModal onlick function
                 smallPosterPath=JSON.stringify(smallPosterPath);
                 let title= JSON.stringify(data.results[i].original_title);
                 let overview= JSON.stringify(data.results[i].overview);
@@ -101,19 +105,27 @@ let findMovies= function (){
     });
 }
 
-//onclick function so that when you click in the background of the modal it exits out. Or if you click the x button on the modal
+//onclick function so that when you click in the background of the modal it exits out.
 window.onclick= function(event){
     if (event.target.className== 'modal-background'){
         movieModal.setAttribute("style", "display:none");
     }
 }
+// Or if you click the x button on the modal it also exits out. 
 exitModal.onclick= function(){
     movieModal.setAttribute("style", "display:none");
 }
 
+// intializing key to 0. If there is something in local storage it gets the length of local storage. 
+let key=0;
+if(localStorage){
+    key=localStorage.length;
+}                                                  
+
 //record favorite function. Sets poster path into local storage
 let recordFavorite=function(smallPosterPath){
     
+    //if the poster path is already in local storage do no set a duplicate into local storage
     for(let i=0; i<localStorage.length; i++){
         if (localStorage.getItem(localStorage.key(i))==smallPosterPath){
             return;
@@ -121,10 +133,9 @@ let recordFavorite=function(smallPosterPath){
     }
     localStorage.setItem(key, smallPosterPath);
     key++;
-
-
 }
 
+// function that checks the availability of the movie on a streaming service
 availabilityButton.addEventListener("click", function(){
 
     let id=favoriteButton.getAttribute("data-id");
@@ -138,17 +149,30 @@ availabilityButton.addEventListener("click", function(){
     .then(response => response.json())
     .then(function(data){
             console.log(data);
-            if(data.streamingInfo.hasOwnProperty("hbo")){
+            // if the movie is on a specific streaming platform the movie logo display is set to true. 
+            let hboBoolean= data.streamingInfo.hasOwnProperty("hbo");
+            let netflixBoolean= data.streamingInfo.hasOwnProperty("netflix");
+            let primeBoolean= data.streamingInfo.hasOwnProperty("prime");
+            let disneyBoolean= data.streamingInfo.hasOwnProperty("disney");
+            let huluBoolean= data.streamingInfo.hasOwnProperty("hulu");
+
+            // if the movie is on a specific streaming platform the movie logo display is set to true. 
+            if(hboBoolean){
                 hbo.setAttribute("style", "display: block");
-            }else if (data.streamingInfo.hasOwnProperty("netflix")){
+            }
+            if (netflixBoolean){
                 netflix.setAttribute("style", "display: block");
-            }else if (data.streamingInfo.hasOwnProperty("prime")){
+            }
+            if (primeBoolean){
                 prime.setAttribute("style", "display: block");
-            }else if(data.streamingInfo.hasOwnProperty("disney")){
+            }
+            if(disneyBoolean){
                 disney.setAttribute("style", "display: block");
-            }else if(data.streamingInfo.hasOwnProperty("hulu")){
+            }
+            if(huluBoolean){
                 hulu.setAttribute("style", "display: block");
-            }else{
+            }
+            if(!hboBoolean && !netflixBoolean && !primeBoolean && !disneyBoolean && !huluBoolean){
                 none.setAttribute("style", "display: block");
             }
     
