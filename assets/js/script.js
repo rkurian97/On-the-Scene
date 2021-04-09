2b6660016a6232d6f48e8309343a616fb9a7b321
dakota <kotalilyy@gmail.com>
2021-04-09 13:57:29 -0500

    add dark/light mode sun & moon

--- assets/js/script.js
+++ assets/js/script.js
@@ -276,7 +276,9 @@ fetch(`${baseURL}trending/movie/week${apiKey}`)
         });
 
 // function for dark mode
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
